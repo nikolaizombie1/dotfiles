2a018dbb73865d31c9101e8e4a8a14c5a29e640c
nikolaizombie1 <fabio.matos999@gmail.com>
2021-08-04 19:44:16 -0400

    Added documentation to the printfrq.sh script

--- Laptop dotfiles/Scripts/printfrq.sh
+++ Laptop dotfiles/Scripts/printfrq.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Author: Fabio J. Matos Nieves
+# Date Created: 4/8/2021
+# Last Modified: 4/8/2021
+
+# Description
+# This script uses cpupower to print out the current CPU frequency (probably only works for intel CPUs)
+
+# Usage
+# ./printfrq.sh
+
 freq=$(sudo cpupower frequency-info | grep -e "current CPU frequency: [0-9]" | cut -d ' ' -f 6)
 unit=$(sudo cpupower frequency-info | grep -e "current CPU frequency: [0-9]" | cut -d ' ' -f 7)
 if [ $unit = "GHz" ]; then
