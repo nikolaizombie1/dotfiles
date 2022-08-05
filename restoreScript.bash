250c5ec0f2c4cd41d6166ce12b3a75db983cfb49
nikolaizombie1 <fabio.matos999@gmail.com>
2022-08-05 16:44:29 -0400

    Added xinit autologin

--- restoreScript.bash
+++ restoreScript.bash
@@ -9,3 +9,4 @@ cp .zshrc ~/
 cp alacritty.yml ~/
 cp starship.toml ~/.config
 cp .xinitrc ~/
+sudo mkdir "/etc/systemd/system/getty@tty1.service.d/" ; sudo mv override.conf /etc/systemd/system/getty@tty1.service.d/
