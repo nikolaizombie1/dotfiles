ed24531faf5d120ff39a1ab2eb9de61a0d1de32c
nikolaizombie1 <fabio.matos999@gmail.com>
2022-10-11 18:15:16 -0400

    Fixed sshfs in autostart.sh

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -10,5 +10,5 @@ picom --config ~/.config/awesome/picom.conf --experimental-backends &
 /usr/bin/emacs --daemon &
 fcitx -d &
 /usr/bin/numlockx on
-sshfs -p 39801 weeb@weeb:/home/weeb/Mass-Storage/ Debian\ Server -C &
+sshfs -p 39801 weeb@weeb:/home/weeb/Mass-Storage/ Debian\ Server -sshfs -p 39801 weeb@weeb:/home/weeb/Mass-Storage/ Debian\ Server -C -o IdentityFile=~/.ssh/id_ed25519_personal &
 blueberry-tray &
