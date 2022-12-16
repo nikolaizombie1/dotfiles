c6db63d1401266f538ed966a458be6476f902703
nikolaizombie1 <70602908+nikolaizombie1@users.noreply.github.com>
2022-12-16 11:11:17 -0400

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -11,16 +11,18 @@ alias ll="exa -lma -s=changed --icons"
 alias waifu2x="waifu2x-ncnn-vulkan"
 alias Music="cd /home/uwu/Linux-Storage/Downloads/Music/"
 alias c='clear'
-alias doom="./.emacs.d/bin/doom"
-alias wd="wol 44:8A:5B:CE:75:8A"
+alias doom="~/./.emacs.d/bin/doom"
+alias wd="wakeonlan 44:8A:5B:CE:75:8A"
 alias vim="nvim"
 
 set PATH $PATH "/var/lib/snapd/snap/bin"
+set PATH $PATH "/usr/sbin"
 set -x DEBUGINFOD_URLS 'https://debuginfod.archlinux.org'
+keychain --eval (command ls ~/.ssh/ | awk '/^id[_a-z0-9]*$/') -q | source
 
 #Start X at login
-#if status is-login
-#    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-#        exec startx -- -keeptty
-#    end
-#end
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
