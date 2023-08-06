24588d41ae7e178d1cc36b178591623e85aae618
nikolaizombie1 <fabio.matos999@gmail.com>
2023-08-06 12:02:23 -0400

    Fixed global hotkeys

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -12,3 +12,4 @@ fcitx5 -d &
 nm-applet &
 /./usr/bin/lxqt-policykit-agent &
 sleep 5s && nmap 10.0.0.0/24 && sshfs weeb@$(ip neigh | awk '/44:8a:5b:ce:75:8a/ {print $1}'):/home/weeb/Mass-Storage ~/Debian\ Server/ -p 39801 -C -o IdentityFile=~/.ssh/id_ed25519_personal &
+xmodmap ~/.Xmodmap &
