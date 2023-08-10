162ad3e6b32b28701803ada1d9fee1f7658880e1
nikolaizombie1 <fabio.matos999@gmail.com>
2023-08-09 21:07:12 -0400

    Started migration to use-package, needs bug fixing

--- emacs/init.el
+++ emacs/init.el
@@ -1,1 +1,14 @@
 (org-babel-load-file "~/.config/emacs/README.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(async flycheck-pos-tip flycheck pdf-tools format-all org-bullets toc-org general rainbow-delimiters latex-preview-pane auctex lua-mode fish-mode magit exec-path-from-shell vterm rustic company-mode yasnippet-snippets which-key use-package smex lsp-ui lsp-ivy ivy-yasnippet fullframe flyspell-correct-ivy evil-collection doom-themes doom-modeline dashboard dap-mode counsel-projectile company all-the-icons-ivy-rich all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
