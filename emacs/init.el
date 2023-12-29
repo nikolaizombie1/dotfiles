bc16124d2887d409928bbe856f808b286087d980
nikolaizombie1 <fabio.matos999@gmail.com>
2023-12-29 13:32:03 -0500

    Add plantuml, haskel, undo tree, yaml and more bindings

--- emacs/init.el
+++ emacs/init.el
@@ -5,7 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(plantuml-mode pyvenv dap-mode doom-modeline python-mode ivy-yasnippet ivy-yasnipet yasnippet-snippets yasnippet yasnipet yasnnipet flyspell-correct-ivy org-bullets toc-org magit fullframe format-all exec-path-from-shell vterm counsel-projectile projectile auctex latex-preview-pane latexpreview-pane pdf-tools rainbow-delimiters all-the-icons-ivy-rich nerd-icons all-the-icons-dired all-the-icons doom-themes which-key use-package rustic lsp-ui lsp-treemacs lsp-ivy ivy-rich helpful general evil-collection counsel company-box)))
+   '(haskell-mode undo-tree plantuml-mode pyvenv dap-mode doom-modeline python-mode ivy-yasnippet ivy-yasnipet yasnippet-snippets yasnippet yasnipet yasnnipet flyspell-correct-ivy org-bullets toc-org magit fullframe format-all exec-path-from-shell vterm counsel-projectile projectile auctex latex-preview-pane latexpreview-pane pdf-tools rainbow-delimiters all-the-icons-ivy-rich nerd-icons all-the-icons-dired all-the-icons doom-themes which-key use-package rustic lsp-ui lsp-treemacs lsp-ivy ivy-rich helpful general evil-collection counsel company-box)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
